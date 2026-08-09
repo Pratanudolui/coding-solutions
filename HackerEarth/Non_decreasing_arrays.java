bfcb5a6f1d38e90405dd54786ce39c4728f6cee4
Pratanudolui <153311818+Pratanudolui@users.noreply.github.com>
2026-08-09 22:58:00 +0530

    Add Non_decreasing_arrays.java implementation
    Implement a solution for non-decreasing arrays based on input cases.

---
+++ HackerEarth/Non_decreasing_arrays.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class TestClass {
+    public static void main(String args[] ) throws Exception {
+       Scanner s = new Scanner(System.in);
+       int cases = s.nextInt();
+       for(int c=0; c<cases; c++) {
+           int N = s.nextInt();
+           long arr = s.nextLong();
+           long ans = arr;
+           StringBuilder sb = new StringBuilder();
+           sb.append(ans + " ");
+           for(int i=1; i<N; i++) {
+               arr = s.nextLong();
+               long temp = ans;
+               if(temp % arr == 0) {
+                   ans = temp;
+               }
+               else {
+                   if(temp < arr) {
+                       ans = arr;
+                   }
+                   else {
+                       ans = temp + arr - (temp % arr);
+                   }
+               }
+               sb.append(ans + " ");
+           }
+           System.out.println(sb.toString());
+       }
+   }
+}
