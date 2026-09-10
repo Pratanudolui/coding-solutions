16d11a307b0b45d6ac9abac33b59cde81d6af387
Pratanudolui <153311818+Pratanudolui@users.noreply.github.com>
2026-09-10 21:36:38 +0530

    Implement averageOfSubtree method in new file

---
+++ LeetCode/2265_Count_Nodes_Equal_to_Average_of_Subtree.java
@@ -0,0 +1,15 @@
+int result = 0;
+    public int averageOfSubtree(TreeNode root) {
+        helper(root);
+        return result;
+    }
+    public int[] helper(TreeNode root){
+        if(root==null)return new int[]{0,0};
+        int[] left = helper(root.left);
+        int[] right = helper(root.right);
+        int sum = left[0]+right[0]+root.val;
+        int count = left[1]+right[1]+1;
+        int average = sum/count;
+        if(average == root.val)result++;
+        return new int[]{sum,count};
+    }
